0bf08f31febf6661fd314bf25d7142485636064a
bongdaehyun <pkjeogus@gmail.com>
2024-08-27 16:44:07 +0900

    test : 메뉴 수정 API test code #13

--- src/main/java/com/sparta/ordersystem/order/management/Menu/dto/MenuResponseDto.java
+++ src/main/java/com/sparta/ordersystem/order/management/Menu/dto/MenuResponseDto.java
@@ -1,10 +1,12 @@
 package com.sparta.ordersystem.order.management.Menu.dto;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.UUID;
 
 @Builder
+@Data
 public class MenuResponseDto {
     UUID menu_id;
     UUID store_id;
--- src/test/java/com/sparta/ordersystem/order/management/Menu/MenuServiceTest.java
+++ src/test/java/com/sparta/ordersystem/order/management/Menu/MenuServiceTest.java
@@ -1,5 +1,7 @@
 package com.sparta.ordersystem.order.management.Menu;
 
+import com.sparta.ordersystem.order.management.Menu.dto.MenuResponseDto;
+import com.sparta.ordersystem.order.management.Menu.dto.UpdateRequestDto;
 import com.sparta.ordersystem.order.management.Menu.entity.Menu;
 import com.sparta.ordersystem.order.management.Menu.repository.MenuRepository;
 import com.sparta.ordersystem.order.management.Menu.service.MenuService;
@@ -72,6 +74,66 @@ public class MenuServiceTest {
         menuService.deleteMenu(menuId);
 
         verify(menuRepository, times(1)).save(menu);
+
+    }
+
+    @Test
+    @DisplayName("메뉴 수정 - 존재하지 않는 메뉴 ID 실패케이스")
+    void testErrorUpdateMenuNotExistedMenuId(){
+        UUID MenuId = UUID.randomUUID();
+        Menu menu = Menu.builder()
+                .menuId(MenuId)
+                .menu_name("test")
+                .content("test")
+                .cost(10000)
+                .build();
+
+        UpdateRequestDto dto = UpdateRequestDto.builder()
+                .menu_name("updateTest")
+                .content("updateTest")
+                .cost(20000)
+                .store_id(UUID.randomUUID())
+                .build();
+
+        String expectedMessage = "존재하지 않는 메뉴 ID";
+
+        given(menuRepository.findByMenuIdAndAndIsActiveTrue(MenuId)).willReturn(Optional.empty());
+        given(messageSource.getMessage("not.found.menu.id",new UUID[]{MenuId},"존재하지 않는 메뉴 ID",
+                Locale.getDefault())).willReturn(expectedMessage);
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+        () -> menuService.updateMenu(dto,MenuId));
+
+        assertEquals(exception.getMessage(),expectedMessage);
+    }
+
+    @Test
+    @DisplayName("메뉴 수정 - 성공 케이스")
+    void testSuccessUpdateMenu(){
+
+        UUID menuId = UUID.randomUUID();
+        Menu menu = Menu.builder()
+                .menuId(menuId)
+                .menu_name("test")
+                .content("test")
+                .cost(10000)
+                .build();
+
+        UpdateRequestDto dto = UpdateRequestDto.builder()
+                .menu_name("updateTest")
+                .content("updateTest")
+                .cost(20000)
+                .store_id(UUID.randomUUID())
+                .build();
+
+        given(menuRepository.findByMenuIdAndAndIsActiveTrue(menuId)).willReturn(Optional.of(menu));
+
+        given(menuRepository.save(menu)).willReturn(menu);
+
+        MenuResponseDto newMenu = menuService.updateMenu(dto,menuId);
+
+        assertEquals(dto.getMenu_name(),newMenu.getMenu_name());
+        assertEquals(menuId,newMenu.getMenu_id());
 
     }
 }
