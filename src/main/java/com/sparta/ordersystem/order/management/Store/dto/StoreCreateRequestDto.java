926a0c8c985616bd35c0f2fbf86124002fa58954
JianBBB <sbganne@naver.com>
2024-08-28 16:28:11 +0900

    fix : UUID에 대한 검증 어노테이션 변경 (@NotBlank -> @NotNull) issue : #8

--- src/main/java/com/sparta/ordersystem/order/management/Store/dto/StoreCreateRequestDto.java
+++ src/main/java/com/sparta/ordersystem/order/management/Store/dto/StoreCreateRequestDto.java
@@ -6,6 +6,7 @@ import com.sparta.ordersystem.order.management.Region.entity.Region;
 import com.sparta.ordersystem.order.management.Store.entity.Store;
 import com.sparta.ordersystem.order.management.User.entity.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,9 +19,9 @@ import java.util.UUID;
 public class StoreCreateRequestDto {
     @NotBlank(message = "storeName cannot be blank")
     private String storeName;
-    @NotBlank(message = "categoryId cannot be blank")
+    @NotNull(message = "categoryId cannot be blank")
     private UUID categoryId;
-    @NotBlank(message = "regionId cannot be blank")
+    @NotNull(message = "regionId cannot be blank")
     private UUID regionId;
 
 
