d4f54b3bb129c3396ced15867db2639b769a80f5
bongdaehyun <pkjeogus@gmail.com>
2024-08-29 11:00:26 +0900

    test : 결제 상세 조회 기능 테스트코드 구현 #27

--- src/test/java/com/sparta/ordersystem/order/management/Payment/PaymentServiceTest.java
+++ src/test/java/com/sparta/ordersystem/order/management/Payment/PaymentServiceTest.java
@@ -3,6 +3,7 @@ package com.sparta.ordersystem.order.management.Payment;
 import com.sparta.ordersystem.order.management.Order.entity.Order;
 import com.sparta.ordersystem.order.management.Order.repository.OrderRepository;
 import com.sparta.ordersystem.order.management.Payment.dto.CreatePaymentRequestDto;
+import com.sparta.ordersystem.order.management.Payment.dto.PaymentResponseDto;
 import com.sparta.ordersystem.order.management.Payment.entity.Payment;
 import com.sparta.ordersystem.order.management.Payment.entity.PaymentMethod;
 import com.sparta.ordersystem.order.management.Payment.entity.PaymentStatus;
@@ -77,5 +78,41 @@ class PaymentServiceTest {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> paymentService.cretePayment(dto));
         // Then
         assertEquals("존재하지 않는 주문 ID", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("결제 내역 상세 조회 - 성공케이스")
+    void testSuccessGetPayment(){
+        UUID paymentId = UUID.randomUUID();
+        UUID orderId = UUID.randomUUID();
+
+        Payment payment = Payment.builder()
+                .paymentId(paymentId)
+                .order(Order.builder().orderId(orderId).build())
+                .build();
+
+        given(paymentRepository.findById(paymentId)).willReturn(Optional.of(payment));
+
+        PaymentResponseDto dto = paymentService.getPaymentsInDetail(paymentId);
+
+        assertEquals(paymentId, dto.getPaymentId());
+        assertEquals(orderId, dto.getOrderId());
+    }
+
+    @Test
+    @DisplayName("결제 내역 상세 조회 - 존재하지 않는 결제 ID 실패케이스")
+    void testErrorGetPaymentNotExistedPaymentId(){
+        UUID paymentId = UUID.randomUUID();
+        String expectedMessage = "존재하지 않는 결제 ID입니다.";
+
+        given(paymentRepository.findById(paymentId)).willReturn(Optional.empty());
+        given(messageSource.getMessage("not.found.payment.id",new UUID[]{paymentId},
+                "존재하지 않는 결제 ID입니다.",
+                Locale.getDefault())).willReturn(expectedMessage);
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> paymentService.getPaymentsInDetail(paymentId));
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
