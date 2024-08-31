b4c5f42f300cc1d317649c4b8fbd556c35bf5eba
JianBBB <sbganne@naver.com>
2024-09-01 00:09:19 +0900

    fix : 카테고리 테스트 코드를 위해, 글로벌 예외에 MethodArgumentNotValidException 추가 및 접근 권한 StatusCode 수정

--- src/main/java/com/sparta/ordersystem/order/management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/ordersystem/order/management/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import com.sparta.ordersystem.order.management.Payment.exception.PaymentNotFound
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -93,12 +94,23 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<RestApiException> AccessDeniedExceptionHandler(AccessDeniedException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.FORBIDDEN.value());
         return new ResponseEntity<>(
                 // HTTP body
                 restApiException,
                 // HTTP status code
                 HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<RestApiException> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex){
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+                // HTTP body
+                restApiException,
+                // HTTP status code
+                HttpStatus.BAD_REQUEST
         );
     }
 
