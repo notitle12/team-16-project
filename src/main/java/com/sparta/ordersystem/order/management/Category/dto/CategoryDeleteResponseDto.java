afe2d7ffe210e292664e464985b217db1af68466
JianBBB <sbganne@naver.com>
2024-09-01 19:17:20 +0900

    fix : CRUD 시 CRUD 시간, CRUD 요청한 사람을 응답값으로 보내도록 수정

--- src/main/java/com/sparta/ordersystem/order/management/Category/dto/CategoryDeleteResponseDto.java
+++ src/main/java/com/sparta/ordersystem/order/management/Category/dto/CategoryDeleteResponseDto.java
@@ -3,6 +3,7 @@ package com.sparta.ordersystem.order.management.Category.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -12,12 +13,16 @@ public class CategoryDeleteResponseDto {
     private final UUID categoryId;
     private final String categoryName;
     private final boolean isActive;
+    private final LocalDateTime deletedAt;
+    private final Long deletedBy;
 
     @Builder
-    public CategoryDeleteResponseDto(UUID categoryId, String categoryName, boolean isActive) {
+    public CategoryDeleteResponseDto(UUID categoryId, String categoryName, boolean isActive, LocalDateTime deletedAt, Long deletedBy) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.isActive = isActive;
+        this.deletedAt = deletedAt;
+        this.deletedBy = deletedBy;
     }
 
 }
--- src/main/java/com/sparta/ordersystem/order/management/Category/dto/CategoryUpdateResponseDto.java
+++ src/main/java/com/sparta/ordersystem/order/management/Category/dto/CategoryUpdateResponseDto.java
@@ -3,17 +3,22 @@ package com.sparta.ordersystem.order.management.Category.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 public class CategoryUpdateResponseDto {
     private final UUID categoryId;
     private final String categoryName;
+    private final LocalDateTime updatedAt;
+    private final Long updatedBy;
 
     @Builder
-    public CategoryUpdateResponseDto(UUID categoryId, String categoryName) {
+    public CategoryUpdateResponseDto(UUID categoryId, String categoryName, LocalDateTime updatedAt, Long updatedBy) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
+        this.updatedAt = updatedAt;
+        this.updatedBy = updatedBy;
     }
 
 }
